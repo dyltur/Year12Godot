c503a75136cb806b6f79ff15d646d09e30cfdabd
dyltur <100171421+dyltur@users.noreply.github.com>
2024-07-01 13:23:22 +1200

    trying to do character moc=vement help

--- Sylvan_trial/main.gd
+++ Sylvan_trial/main.gd
@@ -10,7 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var jump_speed = 5
 func _physics_process(delta):
 	velocity.y += -gravity * delta
-	var input_dir = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var input_dir = Input.get_vector("A","D","W","D")
 	var move_dir = transform.basis * Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if input_dir:
 		velocity.x = move_dir.x * speed
@@ -37,5 +37,8 @@ func _input(event):
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		camera.rotate_x(-event.relative.y * mouse_sensitivity)
 		camera.rotation_degrees.x = clamp(camera.rotation.x,-1,1)
+		rotate_z(-event.relative.y * mouse_sensitivity)
+		camera.rotate_y(-event.relative.x * mouse_sensitivity)
+		camera.rotation_degrees.y = clamp(camera.rotation.y,-1,1)
 
 
