837fa2cda7954ac2c5935777883531644e10fce7
dyltur <100171421+dyltur@users.noreply.github.com>
2024-08-09 14:53:18 +1200

    timer

--- Sylvan_trial/timer.gd
+++ Sylvan_trial/timer.gd
@@ -1,6 +1,24 @@
 extends CanvasLayer
 
+var time = Global.speedrun.time
 
+func _physics_process(delta):
+	time = float(time) + delta
+	
+	update_ui()
+	
+func update_ui():
+	
+	
+	var formatted_time = str(time)
+	var decimal_index = formatted_time.find(".")
+	
+	if decimal_index > 0:
+		formatted_time = formatted_time.left(decimal_index + 3)
+		
+	Global.speedrun.time = formatted_time
+	
+	$label.text = formatted_time 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
