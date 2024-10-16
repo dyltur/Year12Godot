a57995408f95bb7d07f85aa17ad6e62ca0361652
dyltur <100171421+dyltur@users.noreply.github.com>
2024-10-17 10:57:06 +1300

    pushing to home

--- Sylvan_trial/scenes/black_pawn.gd
+++ Sylvan_trial/scenes/black_pawn.gd
@@ -2,10 +2,11 @@ extends MeshInstance3D
 
 
 func black_pawn_move():
-	if chess_move == true:
-		pass
-	else:
-		pass
+	pass
+	#if chess_move == true:
+		#pass
+	#else:
+		#pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
