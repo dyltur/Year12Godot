7903f390190d4ccd03e2c395956e0fdc7001d667
dyltur <100171421+dyltur@users.noreply.github.com>
2024-10-22 14:57:34 +1300

    timer photo

--- Sylvan_trial/gd/timer.gd
+++ Sylvan_trial/gd/timer.gd
@@ -1,29 +1,29 @@
 extends CanvasLayer
 
-#var time = Global.speedrun.time
+var time = Global.speedrun.time
 
-#func _physics_process(delta):
-#	time = float(time) + delta
+func _physics_process(delta):
+	time = float(time) + delta
 	
-#	update_ui()
+	update_ui()
 	
-#func update_ui():
+func update_ui():
 	
 	
-#	var formatted_time = str(time)
-#	var decimal_index = formatted_time.find(".")
+var formatted_time = str(time)
+var decimal_index = formatted_time.find("."):
 	
-#	if decimal_index > 0:
-#		formatted_time = formatted_time.left(decimal_index + 3)
+	if decimal_index > 0:
+		formatted_time = formatted_time.left(decimal_index + 3)
 		
-#	Global.speedrun.time = formatted_time
+		Global.speedrun.time = formatted_time
 	
-#	$label.text = formatted_time 
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
+		$label.text = formatted_time 
+ #Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
